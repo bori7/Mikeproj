e7d591673aef51d7cf26efe29387a62c48e076b3
Bori7 <oluwaboriife@gmail.com>
2020-11-17 07:53:06 +0100

    Updates

---
+++ precache-manifest.c6139b0abfda8d7eb162778a965e3022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f850825bbe6c280d9d0c12d49d64195a",
+    "url": "/Mikeproj/index.html"
+  },
+  {
+    "revision": "b7b56f911784b673b451",
+    "url": "/Mikeproj/static/css/main.e3b74797.chunk.css"
+  },
+  {
+    "revision": "3171835d4af35eebf177",
+    "url": "/Mikeproj/static/js/2.80ecb236.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/Mikeproj/static/js/2.80ecb236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b56f911784b673b451",
+    "url": "/Mikeproj/static/js/main.c8ec9b3c.chunk.js"
+  },
+  {
+    "revision": "2e1e988f9f4f336de9b6",
+    "url": "/Mikeproj/static/js/runtime-main.121c47ee.js"
+  }
+]);
